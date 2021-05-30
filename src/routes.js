1b1387a041ea1e8cc666b91a3abc54d02c76ae7d
O Daniel <soumenorgame@gmail.com>
2021-05-30 11:08:47 -0300

    Sistema de mensagem funcioanndo

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const authMiddleware = require("./middleware/authorization");
 
+
 const messageController = require("./controller/message/message");
 
 const routes = express.Router();
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const app = require("./app");
 const socket = require("socket.io");
+const Message = require("./models/Message");
 
 
 const PORT = 3002;
@@ -10,7 +11,7 @@ const server = app.listen("3002", () => {
 
 const io = socket(server);
 
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
     console.log(socket.id);
 
     socket.on("join_room", (data) => {
@@ -18,13 +19,34 @@ io.on("connection", (socket) => {
         console.log("User Joined Room: " + data);
     });
 
-    socket.on("send_message", (data) => {
+    socket.on("send_message", async (data) => {
         console.log(data);
-        socket.to(data.room).emit("receive_message", data.content);
+        console.log(data.content.message);
+            const message = await Message.create({
+                text: data.content.message,
+                userId: data.content.userId,
+                groupId: data.content.groupId,
+                chatId: data.content.chatId,
+            });
+            socket.to(data.room).emit("receive_message", data.content);
     });
 
     socket.on("disconnect", () => {
         console.log("USER DISCONNECTED");
     });
+
+    socket.on("delete_message", async (data) => {
+
+        const message = await Message.findOne({
+            where: {
+                id : data.content.id,
+                userId : data.content.userId
+            }
+        });
+
+        if(!message)
+            return res.status(401).send({ error: "Mensagem não encontrada"})
+        message.destroy();    
+    })
 });
 
