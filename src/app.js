a678b49649920a35852eb837d69e9447a8183397
O Daniel <soumenorgame@gmail.com>
2021-05-30 09:40:55 -0300

    Melhorando o servidor

--- src/app.js
+++ src/app.js
@@ -1,22 +1,13 @@
 //import do express
 const express = require("express");
-
-const { errors } = require("celebrate");
-
 const cors = require("cors");
 
-const routes = require("./routes");
-
-require("./database");
+//require("./database");
 
 const app = express();
 
 app.use(express.json());
 
 app.use(cors());
-
-app.use(routes);
-
-app.use(errors());
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,29 @@
 const app = require("./app");
+const socket = require("socket.io");
+
 
 const PORT = 3002;
 
-app.listen(PORT, () => {
+const server = app.listen("3002", () => {
     console.log(`Servidor rodando na porta ${PORT}`)
-})
+});
+
+io = socket(server);
+
+io.on("connection", (socket) => {
+    console.log(socket.id);
+
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log("User Joined Room: " + data);
+    });
+
+    socket.on("send_message", (data) => {
+        console.log(data);
+        socket.to(data.room).emit("receive_message", data.content);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("USER DISCONNECTED");
+    });
+});
