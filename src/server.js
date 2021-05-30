9dc0c761d9539506c65aab3f78cecd167769d346
O Daniel <soumenorgame@gmail.com>
2021-05-30 11:19:49 -0300

    Melhorias

--- src/server.js
+++ src/server.js
@@ -20,13 +20,13 @@ io.on("connection", async (socket) => {
     });
 
     socket.on("send_message", async (data) => {
-        console.log(data);
-        console.log(data.content.message);
+        // console.log(data);
+        // console.log(data.content.message);
             const message = await Message.create({
                 text: data.content.message,
-                userId: data.content.userId,
-                groupId: data.content.groupId,
-                chatId: data.content.chatId,
+                userId: data.userId,
+                groupId: data.groupId,
+                chatId: data.chatId,
             });
             socket.to(data.room).emit("receive_message", data.content);
     });
